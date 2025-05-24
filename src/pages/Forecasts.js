13080f8b94593009ffe7035ad37b17ea306b9dbd
Kishore094 <kishorekanagaraj@gmail.com>
2025-05-24 13:36:22 +0100

    Create Forecasts.js

---
+++ src/pages/Forecasts.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default function Forecasts() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Forecasts</h1>
+      <div className="bg-gray-900 rounded-2xl p-8 shadow border border-gray-800">
+        <div className="text-lg">Forecasting insights will be shown here.</div>
+      </div>
+    </div>
+  );
+}
