88414bd6baee9c6bf2bd710e85296486a526b010
Kishore094 <kishorekanagaraj@gmail.com>
2025-05-25 17:35:47 +0100

    Update Forecasts.js

--- src/pages/Forecasts.js
+++ src/pages/Forecasts.js
@@ -1,10 +1,11 @@
 import React from "react";
+
 export default function Forecasts() {
   return (
     <div>
-      <h1 className="text-3xl font-bold mb-4">Forecasts</h1>
-      <div className="bg-gray-900 rounded-2xl p-8 shadow border border-gray-800">
-        <div className="text-lg">Forecasting insights will be shown here.</div>
+      <h1 className="text-3xl font-bold mb-4 text-gray-900 dark:text-white">Forecasts</h1>
+      <div className="bg-white dark:bg-gray-900 rounded-xl p-8 shadow border border-gray-200 dark:border-gray-800">
+        <div className="text-lg text-gray-900 dark:text-white">Forecasting insights will be shown here.</div>
       </div>
     </div>
   );
